fb026450c0bb13b17d0571609631a9a9ff9b8a08
Anikett009 <aniketpethe007@gmail.com>
2024-07-02 21:01:45 +0530

    npm i

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { FC } from "react"
-import L from "leaflet"
-import { MapContainer, Marker, TileLayer } from "react-leaflet"
+"use client";
+import { FC } from "react";
+import L from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css"
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
@@ -23,13 +23,12 @@ interface MapProps {
 const Map: FC<MapProps> = ({ center }) => {
   return (
     <MapContainer
-      center={(center as L.LatLngExpression) || [51, -0.9]}
+     center={center as L.LatLngExpression || [51, -0.9]}
       zoom={center ? 4 : 2}
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {center && <Marker position={center as L.LatLngExpression} />}
